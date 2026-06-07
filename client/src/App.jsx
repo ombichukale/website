d0b998c3e0f182b47df1cb5e67124fcbc690b6d0
om bichukale <bichukaleom2003@gmail.com>
2026-06-07 13:59:28 +0530

    tailwind setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1>AI Website Builder</h1>
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-white">
+        AI Website Builder
+      </h1>
     </div>
   )
 }
